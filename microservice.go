b79adb56c22d5b4645994a2a9339429b08695bc8
Jack <jack@toumey.io>
2019-05-18 00:23:58 -0700

    web server is working

--- microservice.go
+++ microservice.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Print("Implement me.")
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hellllllllloooooo world")
 }
