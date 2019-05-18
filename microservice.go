bcddcc66350b3da26ea476ce8ae413fee69edd5a
Jack <jack@toumey.io>
2019-05-18 00:40:47 -0700

    adding parameter response to api

--- microservice.go
+++ microservice.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/api/echo", echo)
 	http.ListenAndServe(port(), nil)
 }
 
@@ -16,10 +17,18 @@ func port() string {
 	if len(port) == 0 {
 		port = "8080"
 	}
-	return ":" + port
+
+	// adding localhost to avoid getting the MacOS firewall notification
+	return "localhost:" + port
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Hellllllllloooooo world")
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Query()["message"][0]
+	w.Header().Add("Content-Type", "text/plain")
+	fmt.Fprintf(w, message)
 }
